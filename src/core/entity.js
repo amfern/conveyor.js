393c9645dd9d62e405e74837383b328e98dd44c2
amfern <amfernusus@gmail.com>
2013-10-02 23:12:11 +0000

    add system and entity, beef engine overwall skeleton

---
+++ src/core/entity.js
@@ -0,0 +1,8 @@
+
+window.Entity = (function(config) {
+  // set defaults
+  config = config || {};
+  config.components = config.components || []; // collection of all components associated with this entity
+
+  return comp.registerEntity(config);
+})();
---
+++ src/core/system.js
@@ -0,0 +1,16 @@
+
+window.System = (function(config) {
+  // set defaults
+  config = config || {};
+  config.dependencies = config.dependencies || []; // systems that should run before this one
+  config.component = config.component || function() { return {}; }; // new component generator for this system
+  config.component = config.component || function() { return {}; }; // new component generator for this system
+
+  /*
+  * The heart of the system where entities are proccessed.
+  * whilte loop that loops througe all components of entities
+  */
+  if(typeof(config.proccess != 'function')) throw 'proccess function is mandatory'; 
+
+  return comp.registerSystem(config.name, config);
+})();
