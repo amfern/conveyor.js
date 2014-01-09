e21fccdee6e15789595ca8e0b8967399aa14c14c
amfern <amfernusus@gmail.com>
2014-01-09 00:23:36 +0000

    -refactore mouse state -add tests to mouse state -split tests into system and Zcore test

--- src/systems/IO/input/basic/MouseState.js
+++ src/systems/IO/input/basic/MouseState.js
@@ -2,39 +2,61 @@
 // Analog states will be passed as {mouseMoved: true, mouseMovedUp: true, mouseMoveY: -0.73}
 // -----------------------------------------
 (function() {
-  var state = {}, bufferState;
+  var state = {}, prevMovementBuffer, movementBuffer, buttonBuffer = {};
 
   function init() {
-    var element = COMP.getDOMElement();
-    resetBuffer(); // reset buffer
+    // set initial state
+    movementBuffer = {
+      movementX: 0,
+      movementY: 0,
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0
+    };
+
+    // set initial prevMovementBuffer
+    prevMovementBuffer = {
+      screenX: 0,
+      screenY: 0,
+      clientX: 0,
+      clientY: 0
+    };
+
+    var element = window.document;
 
     element.addEventListener('mousemove', function(e) {
-      bufferState.movementX += e.movementX;
-      bufferState.movementY += e.movementY;
-
-      bufferState[mouseMovedUp]    = bufferState.movementY > 0;
-      bufferState[mouseMovedDown]  = bufferState.movementY < 0;
-      bufferState[mouseMovedRight] = bufferState.movementX > 0;
-      bufferState[mouseMovedLeft]  = bufferState.movementX < 0;
-
-      bufferState.clientX += e.clientX;
-      bufferState.clientY += e.clientY;
-      bufferState.screenX += e.screenX;
-      bufferState.screenY += e.screenY;
-
-      bufferState.mouseMoved = !bufferState.movementX && !bufferState.movementY ? false : true;
+      movementBuffer.screenX = e.screenX;
+      movementBuffer.screenY = e.screenY;
+      movementBuffer.clientX = e.clientX;
+      movementBuffer.clientY = e.clientY;
 
       e.preventDefault();
     }, false);
 
     element.addEventListener('mousedown', function(e) {
-      bufferState[e.button] = true;
+      buttonBuffer[e.button] = true;
       e.preventDefault();
     }, false);
   }
 
-  function resetBuffer() {
-    bufferState = {clientX: 0,clientY: 0, screenX: 0, screenY: 0};
+  function processMovementBuffer() {
+    // calculate movement
+    movementBuffer.movementX = movementBuffer.screenX - prevMovementBuffer.screenX;
+    movementBuffer.movementY = movementBuffer.screenY - prevMovementBuffer.screenY;
+
+    // set previous position to current
+    prevMovementBuffer.screenX = movementBuffer.screenX;
+    prevMovementBuffer.screenY = movementBuffer.screenY;
+    prevMovementBuffer.clientX = movementBuffer.clientX;
+    prevMovementBuffer.clientY = movementBuffer.clientY;
+
+    // calculate mouse movementBuffers
+    movementBuffer.mouseMovedUp    = movementBuffer.movementY < 0;
+    movementBuffer.mouseMovedDown  = movementBuffer.movementY > 0;
+    movementBuffer.mouseMovedLeft  = movementBuffer.movementX < 0;
+    movementBuffer.mouseMovedRight = movementBuffer.movementX > 0;
+    movementBuffer.mouseMoved      = !movementBuffer.movementX && !movementBuffer.movementY ? false : true;
   }
 
   init();
@@ -49,9 +71,13 @@
     },
 
     process: function(staticEntity) {
+      processMovementBuffer();
+
       _.clearAll(state);  // clear state
-      _.extend(state, bufferState); // copy bufferState to state
-      resetBuffer(); // reset 
+      _.extend(state, movementBuffer); // copy bufferState to state
+      _.extend(state, buttonBuffer); // copy bufferState to state
+      
+      buttonBuffer = {}; // reset down buttons
     }
   });
 })();
