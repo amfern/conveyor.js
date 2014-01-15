209e4f23b0a87abecff81f0ba703ba8cbb302d70
amfern <amfernusus@gmail.com>
2014-01-15 21:21:36 +0000

    workaround chrome keydown event bug fix mocks bug add mouse state tests

--- spec/systems/IO/basic/MouseState.js
+++ spec/systems/IO/basic/MouseState.js
@@ -3,53 +3,61 @@ describe("mouseState", function() {
 
   // add reading system
   beforeEach(function() {
-    tempSystem = new COMP.System.IO({
-      name: 'EpicSystemReadingMouseState',
-      dependencies: ['MouseState'],
-      component: function() { },
-      process: function(entities) {
-        state = entities[0]['MouseState'];
-      }
-    });
+    COMP.cycleOnce();
+    tapIntoSystem('MouseState', function(s) {state = s;});
   });
 
-  // add entity
-  beforeEach(function() {
-    entity = new COMP.Entity({
-      name: "EpicSystemReadingMouseStateEntity",
-      components: ['EpicSystemReadingMouseState']
-    });
-  });
-
-  // reset mouse to 0,0 position after each test
   afterEach(function() {
-    mouseEvent("mousemove", 0, 0, 0, 0, 0, 0);
+    mouseMoveEvent(0, 0);
+    COMP.cycleOnce();
   });
 
-  describe('mouse movement', function() {
-    beforeEach(function() {
-      evt = mouseMoveEvent(10, 20);
-    });
-    
-    it("should capture mouse move event", function () {
-      COMP.cycleOnce(function() {
-        expect(state).toEqual({
-          mouseMoved: true,
-          mouseMovedUp: false,
-          mouseMovedDown: true,
-          mouseMovedRight: true,
-          mouseMovedLeft : false,
-          movementX: 10,
-          movementY: 20,
-          screenX: 10,
-          screenY: 20,
-          clientX: 0,
-          clientY: 0
-        });
+  it("should capture mouse move event", function () {
+    mouseMoveEvent(10, 20);
+
+    COMP.cycleOnce(function() {
+      expect(state).toEqual({
+        mouseMoved: true,
+        mouseMovedUp: false,
+        mouseMovedDown: true,
+        mouseMovedRight: true,
+        mouseMovedLeft : false,
+        movementX: 10,
+        movementY: 20,
+        screenX: 10,
+        screenY: 20,
+        clientX: 0,
+        clientY: 0
       });
     });
+  });
 
-    it("should reset state each engine cycle", function () {
+  it("should reset state each engine cycle", function () {
+    COMP.cycleOnce(function() {
+      expect(state).toEqual({
+        mouseMoved: false,
+        mouseMovedUp: false,
+        mouseMovedDown: false,
+        mouseMovedRight: false,
+        mouseMovedLeft: false,
+        movementX: 0,
+        movementY: 0,
+        screenX: 0,
+        screenY: 0,
+        clientX: 0,
+        clientY: 0
+      });
+    });
+  });
+    
+  describe('mouse movement', function() {
+    beforeEach(function() {
+      COMP.cycleOnce();
+      evt = mouseMoveEvent(10, 20);
+    });
+
+    it("should updated mouse movement state even if mouse stayed on the same place", function () {
+      COMP.cycleOnce();
       COMP.cycleOnce(function() {
         expect(state).toEqual({
           mouseMoved: false,
@@ -67,50 +75,58 @@ describe("mouseState", function() {
       });
     });
 
-    describe('mouse click', function() {
-      it("should capture mousedown button", function () {
-        evt = mouseClickEvent(0);
-  
-        COMP.cycleOnce(function() {
-          expect(state).toEqual({
-            movementX: 0,
-            movementY: 0,
-            screenX: 10,
-            screenY: 20,
-            clientX: 0,
-            clientY: 0,
-            mouseMovedUp: false,
-            mouseMovedDown:
-            false,
-            mouseMovedLeft: false,
-            mouseMovedRight: false,
-            mouseMoved: false,
-            0: true
-          });
+    it("should run over previous value if engine hasn't looped yet", function () {
+      mouseMoveEvent(1, 45);
+
+      COMP.cycleOnce(function() {
+        expect(state).toEqual({
+          mouseMoved: true,
+          mouseMovedUp: false,
+          mouseMovedDown: true,
+          mouseMovedRight: true,
+          mouseMovedLeft: false,
+          movementX: 1,
+          movementY: 45,
+          screenX: 1,
+          screenY: 45,
+          clientX: 0,
+          clientY: 0
         });
       });
+    });
 
-      it("should reset state each engine cycle", function () {
-        COMP.cycleOnce(function() {
-          expect(state).toEqual({
-            mouseMoved: false,
-            mouseMovedUp: false,
-            mouseMovedDown: false,
-            mouseMovedRight: false,
-            mouseMovedLeft: false,
-            movementX: 0,
-            movementY: 0,
-            screenX: 10,
-            screenY: 20,
-            clientX: 0,
-            clientY: 0
-          });
+    it("should capture mousemove and mousedown", function () {
+      mouseClickEvent(2);
+      mouseClickEvent(1);
+
+      COMP.cycleOnce(function() {
+        expect(state).toEqual({
+          mouseMoved: true,
+          mouseMovedUp: false,
+          mouseMovedDown: true,
+          mouseMovedRight: true,
+          mouseMovedLeft: false,
+          movementX: 10,
+          movementY: 20,
+          screenX: 10,
+          screenY: 20,
+          clientX: 0,
+          clientY: 0,
+          2: true,
+          1: true
         });
+      });
+    });
+
+    it("should prevent default", function () {
+      COMP.cycleOnce(function() {
+        expect(evt.defaultPrevented).toEqual(true);
       });
     });
 
     describe('should capture fresh', function() {
       beforeEach(function() {
+        COMP.cycleOnce();
         evt = mouseMoveEvent(15, 5);
       });
 
@@ -138,36 +154,6 @@ describe("mouseState", function() {
         });
       });
 
-      it("mouse click", function () {
-        evt = mouseClickEvent(2);
-        evt = mouseClickEvent(1);
-
-        COMP.cycleOnce(function() {
-
-          expect(state).toEqual({
-            mouseMoved: false,
-            mouseMovedUp: false,
-            mouseMovedDown: false,
-            mouseMovedRight: false,
-            mouseMovedLeft: false,
-            movementX: 0,
-            movementY: 0,
-            screenX: 15,
-            screenY: 5,
-            clientX: 0,
-            clientY: 0,
-            2: true,
-            1: true
-          });
-        });
-      });
-
-      it("should prevent default", function () {
-        COMP.cycleOnce(function() {
-          expect(evt.defaultPrevented).toEqual(true);
-        });
-      });
-
       // do just a normal event dispatch test with helpers and include clientX and clientY
       it("should fill state correctly", function () {
         evt = mouseEvent("mousemove", 1, 2, 3, 4, 0, 0);
@@ -179,19 +165,61 @@ describe("mouseState", function() {
             mouseMovedDown: false,
             mouseMovedRight: false,
             mouseMovedLeft: true,
-            movementX: -14,
-            movementY: -3,
+            movementX: -9,
+            movementY: -18,
             screenX: 1,
             screenY: 2,
             clientX: 3,
             clientY: 4
           });
         });
-      
       });
-
     });
-
   });
 
+  describe('mouse click', function() {
+    beforeEach(function() {
+      COMP.cycleOnce();
+    });
+
+    it("should capture mousedown button", function () {
+      mouseClickEvent(0);
+
+      COMP.cycleOnce(function() {
+        expect(state).toEqual({
+          movementX: 0,
+          movementY: 0,
+          screenX: 0,
+          screenY: 0,
+          clientX: 0,
+          clientY: 0,
+          mouseMovedUp: false,
+          mouseMovedDown:
+          false,
+          mouseMovedLeft: false,
+          mouseMovedRight: false,
+          mouseMoved: false,
+          0: true
+        });
+      });
+    });
+
+    it("should reset state each engine cycle", function () {
+      COMP.cycleOnce(function() {
+        expect(state).toEqual({
+          mouseMoved: false,
+          mouseMovedUp: false,
+          mouseMovedDown: false,
+          mouseMovedRight: false,
+          mouseMovedLeft: false,
+          movementX: 0,
+          movementY: 0,
+          screenX: 0,
+          screenY: 0,
+          clientX: 0,
+          clientY: 0
+        });
+      });
+    });
+  });
 });
