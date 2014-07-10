3582b3dea72388632454cc6cbc0a7bf9228a8118
amfern <amfern@debian>
2014-07-09 20:02:55 -0400

    fix system and entities not removed from mocked collections if removed during the test

--- spec/mocks/system.js
+++ spec/mocks/system.js
@@ -11,18 +11,34 @@
         COMP.System.mockedSystems.push(this);
     };
     COMP.System.Logic.prototype = logicConstructor.prototype;
+    COMP.System.Logic.prototype.originalRemove = COMP.System.Logic.prototype.remove;
+    COMP.System.Logic.prototype.remove = function() {
+        COMP.System.Logic.prototype.originalRemove.apply(this);
+        COMP.System.mockedSystems.splice(COMP.System.mockedSystems.indexOf(this), 1);
+    };
 
     COMP.System.Interpolate = function () {
         InterpolateConstructor.apply(this, arguments);
         COMP.System.mockedSystems.push(this);
     };
     COMP.System.Interpolate.prototype = InterpolateConstructor.prototype;
+    COMP.System.Interpolate.prototype.originalRemove = COMP.System.Interpolate.prototype.remove;
+    COMP.System.Interpolate.prototype.remove = function() {
+        COMP.System.Interpolate.prototype.originalRemove.apply(this);
+        COMP.System.mockedSystems.splice(COMP.System.mockedSystems.indexOf(this), 1);
+    };
 
     COMP.System.IO = function () {
         IOConstructor.apply(this, arguments);
         COMP.System.mockedSystems.push(this);
     };
     COMP.System.IO.prototype = IOConstructor.prototype;
+    COMP.System.IO.prototype.originalRemove = COMP.System.IO.prototype.remove;
+    COMP.System.IO.prototype.remove = function() {
+        COMP.System.IO.prototype.originalRemove.apply(this);
+        COMP.System.mockedSystems.splice(COMP.System.mockedSystems.indexOf(this), 1);
+    };
+
 
     COMP.System.mockedSystems = [];
 })();
